6dded35ef9f53f549d2a099ac1c6561e38c6be46
Shubham Kumar <shubham35godda@gmail.com>
2022-02-19 19:44:52 +0530

    updated backend
    updated backend

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
-//pM5bfCo7JmV8yAqD
-const express=require ('express');
-const mongoose=require("mongoose");
-const router=require("./routes/bookroute");
-const app =express();
+const express = require("express");
+const mongoose = require("mongoose");
+const router = require("./routes/book-routes");
+const cors = require("cors");
+const app = express();
+
+// Middlewares
 app.use(express.json());
+app.use(cors());
+app.use("/books", router); // localhost:5000/books
 
 mongoose.connect("mongodb+srv://admin:pM5bfCo7JmV8yAqD@cluster0.f5kt4.mongodb.net/BookList?retryWrites=true&w=majority").then(()=>console.log("Connected to database")).then(()=>{
     app.listen(5000);
 }).catch((err)=>console.log(err));
-app.use("/books",router);
